7aa8434656c8a62450a445049f840871bd73f4ab
Sujit Thakur <tsujeet440@gmail.com>
2025-09-20 19:24:00 +0530

    Product Cotrol Added as Component Only Frontend

---
+++ src/components/ProductControl/ProductControl.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Image from "next/image";
+
+const ProductControl = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-between bg-[#F6F7F8] p-2 mt-3 mb-3 gap-4 rounded">
+      <div className="flex flex-col md:flex-row items-center gap-15 w-full px-2">
+        <span className="text-[#22262A] text-sm font-medium">
+          13 Items
+        </span>
+
+        <div className="flex items-center gap-5">
+          <label
+            htmlFor="sortBy"
+            className="text-[#22262A] text-sm font-medium"
+          >
+            Sort By
+          </label>
+          <select
+            id="sortBy"
+            className="border-2 border-[#F1F3F4] rounded px-2 py-2 text-sm w-[142px]"
+          >
+            <option value="Name">Name</option>
+            <option value="Price">Price</option>
+            <option value="Date">Date</option>
+          </select>
+        </div>
+        <div className="flex items-center gap-2">
+          <label
+            htmlFor="showCount"
+            className="text-[#22262A] text-sm font-medium  "
+          >
+            Show
+          </label>
+          <select
+            id="showCount"
+            className=" rounded px-2 py-2 text-sm w-[142px] border-2 border-[#F1F3F4]"
+          >
+            <option value={6}>6</option>
+            <option value={12}>12</option>
+            <option value={24}>24</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <button className="p-2 rounded text-blue-600 hover:bg-gray-100">
+          <Image
+            src="/images/rectangle.svg"
+            alt="Grid View"
+            width={24}
+            height={21}
+          />
+        </button>
+
+        <button className="p-2 hover:bg-gray-100">
+          <Image src="/images/bar.svg" alt="List View" width={22} height={17} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductControl;
--- src/components/ProductGrid/ProdductGrid.tsx
+++ src/components/ProductGrid/ProdductGrid.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
 import { Product } from "../../types/product";
 import Hero from "../Hero";
+import ProductControl from "../ProductControl/ProductControl";
 
 interface ProductGridProps {
   products: Product[];
@@ -13,6 +14,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
   return (
     <div>
       <Hero />
+      <ProductControl />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
