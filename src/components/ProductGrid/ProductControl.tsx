dec61e8ffc0f63fcd150cc0a8f11b5f02c758153
Sujit Thakur <tsujeet440@gmail.com>
2025-09-21 22:26:23 +0530

    Product controller Improvement Responsive

--- src/components/ProductGrid/ProductControl.tsx
+++ src/components/ProductGrid/ProductControl.tsx
@@ -17,13 +17,13 @@ const ProductControl: React.FC<ProductControlProps> = ({
   onShowCountChange,
 }) => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-[#F6F7F8] p-2 mt-3 mb-3 gap-4 rounded">
-      <div className="flex flex-col md:flex-row items-center gap-15 w-full px-2">
+    <div className="flex flex-col md:flex-row items-start md:items-center justify-between bg-[#F6F7F8] p-2 mt-3 mb-3 gap-4 rounded">
+      <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-6 w-full px-2 flex-wrap">
         <span className="text-[#22262A] text-sm font-medium">
           {totalItems} Items
         </span>
 
-        <div className="flex items-center gap-5">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5 w-full sm:w-auto">
           <label
             htmlFor="sortBy"
             className="text-[#22262A] text-sm font-medium"
@@ -33,16 +33,16 @@ const ProductControl: React.FC<ProductControlProps> = ({
           <select
             id="sortBy"
             value={sortBy}
-            className="border-2 border-[#F1F3F4] rounded px-2 py-2 text-sm w-[142px]"
+            className="border-2 border-[#F1F3F4] rounded px-2 py-2 text-sm w-full sm:w-[142px]"
             onChange={(e) => onSortChange(e.target.value)}
           >
-            <option value="">None</option>
+            <option value="">Select</option>
             <option value="Name">Name</option>
             <option value="Price">Price</option>
           </select>
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 w-full sm:w-auto">
           <label
             htmlFor="showCount"
             className="text-[#22262A] text-sm font-medium"
@@ -51,7 +51,7 @@ const ProductControl: React.FC<ProductControlProps> = ({
           </label>
           <select
             id="showCount"
-            className="rounded px-2 py-2 text-sm w-[142px] border-2 border-[#F1F3F4]"
+            className="rounded px-2 py-2 text-sm w-full sm:w-[142px] border-2 border-[#F1F3F4]"
             onChange={(e) => onShowCountChange(Number(e.target.value))}
           >
             <option value={6}>6</option>
@@ -61,7 +61,7 @@ const ProductControl: React.FC<ProductControlProps> = ({
         </div>
       </div>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 mt-2 md:mt-0">
         <button className="p-2 rounded text-blue-600 hover:bg-gray-100">
           <Image
             src="/images/rectangle.svg"
